13165b1e706766c2e7d5f22160d9527e7428e071
Crescent <trtonoy2@gmail.com>
2026-06-24 17:44:46 +0600

    Analysis

--- test-builds.js
+++ test-builds.js
@@ -1,11 +1,11 @@
 ﻿const jsdom = require('jsdom');
 const { JSDOM } = jsdom;
 const fs = require('fs');
-const html = fs.readFileSync('e:/DEV/Pokemon Champions Guide/teambuilder/index.html', 'utf8');
+const html = fs.readFileSync('e:/DEV/Pokemon Champions Guide/builds/index.html', 'utf8');
 
 const virtualConsole = new jsdom.VirtualConsole();
 virtualConsole.on('error', (...err) => console.error('ERROR:', ...err));
 virtualConsole.on('jsdomError', (...err) => console.error('JSDOM ERROR:', ...err));
 
 const dom = new JSDOM(html, { runScripts: 'dangerously', virtualConsole, url: 'http://localhost' });
-console.log('Teambuilder script executed successfully!');
+console.log('Builds script executed successfully!');
